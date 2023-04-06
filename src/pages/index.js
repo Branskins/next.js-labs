b012e7dc0dca63274bda4a768066b40c60923b79
Andres Jimenez <andresje17@gmail.com>
2023-04-06 00:31:26 -0600

    Test MDX navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,17 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import MyMDXPage from './my-mdx-page.mdx'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <MyMDXPage />
+    <>
+      <MyMDXPage />
+      <Link href="/projects/my-mdx-page2">Page 2</Link>
+      <br></br>
+      <Link href="/projects/my-mdx-page3">Page 3</Link>
+    </>
   )
 }
